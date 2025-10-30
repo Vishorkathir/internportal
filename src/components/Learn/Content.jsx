c517883541e4a6e90630416a82c2b3ff6183c05f
vishor <vishorgunasekaran07@gamil.com>
2025-10-30 16:22:40 +0530

    third commit

--- src/components/Learn/Content.jsx
+++ src/components/Learn/Content.jsx
@@ -66,46 +66,17 @@ const Content = () => {
         {/* content cardtable */}
 
 
-        <Table/>
+        
 
 
-        {/* <div className="border bg-white rounded-xl p-5  ">
+        <div className="border bg-white rounded-xl p-5  ">
           <div>
-            <h1 className="text-2xl font-semibold">Input Datasets</h1>
-            <h1 className="text-gray-500 text-lg mt-1 mb-2">Here are the tables we'll be working with:</h1>
-            <h1 className="text-lg font-bold">Table:Bookings</h1>
+            <h1 className="text-2xl font-semibold">{json.content1}</h1>
+                    <h1 className="text-gray-500 text-lg mt-1 mb-2">{json.content2}</h1>
+                    <h1 className="text-lg font-bold">{json.content3}</h1>
           </div>
 
-          <div className="mt-2  text-center rounded-xl " >
-            <table className="w-full  rounded-xl ">
-              <thead className=" bg-blue-200  text-black  ">
-
-                <tr className="text-2xl">
-                  <th className="p-5  rounded-tl-2xl rounded-bl-2xl font-small " >user_id</th>
-                  <th >user_name</th>
-                  <th >loyalty_tier</th>
-                  <th >boooking_time</th>
-                  <th className="rounded-tr-2xl rounded-br-2xl ">class</th>
-                </tr>
-
-              </thead>
-              <tbody className="text-xl" >
-                {
-                  json.person.map((details, index2) => (
-
-                    <tr key={index2} className=" border-b-1  border-gray-400 rounded-xl text-gray-600 " >
-                      <td className=" pt-9 pb-3">{details.id}</td>
-                      <td className=" pt-9 pb-3">{details.userid}</td>
-                      <td className=" pt-9 pb-3">{details.loyaltier}</td>
-                      <td className=" pt-9 pb-3">{details.booktime}</td>
-                      <td className=" pt-9 pb-3">{details.class}</td>
-                    </tr>
-                  ))
-                }
-
-              </tbody>
-            </table>
-          </div>
+         <Table content={json.table}/>
 
           <div className="" >
             <h1 className="font-bold mt-3 mb-3">
@@ -127,7 +98,7 @@ const Content = () => {
             </table>
           </div>
 
-        </div> */}
+        </div>
 
 
         {/* card4 ->1 */}
@@ -174,11 +145,24 @@ const Content = () => {
                       ))
                     }
                   </div>
+
+                  <div>
+                    {
+
+                    }
+                    </div>
+
+
+ <Table content={json.contcard1[0].table1}/>
+
+
+                    
                 </div>
 
               </div>
             ))
           }
+         
 
         </div>
 
